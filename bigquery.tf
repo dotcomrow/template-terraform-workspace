b75e9fe32f309ad65641531731bafd170da53b17
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-10-15 16:26:28 -0400

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- bigquery.tf
+++ bigquery.tf
@@ -2,14 +2,16 @@ resource "google_bigquery_dataset" "main_dataset" {
   dataset_id                  = "${var.project_name}_dataset"
   description                 = "Dataset for ${var.project_name} project"
   location                    = "US"
-  project                     = var.project_name
+  project                     = var.project_id
+
+  depends_on = [ google_project_service.project_service ]
 }
 
 resource "google_bigquery_table" "sequences" {
   dataset_id = google_bigquery_dataset.main_dataset.dataset_id
   table_id   = "sequences"
   deletion_protection = false
-  project                     = var.project_name
+  project                     = var.project_id
   schema = <<EOF
 [
   {
@@ -26,6 +28,8 @@ resource "google_bigquery_table" "sequences" {
   }
 ]
 EOF
+
+  depends_on = [ google_bigquery_dataset.main_dataset ]
 }
 
 resource "google_bigquery_routine" "get_row_id" {
@@ -33,10 +37,12 @@ resource "google_bigquery_routine" "get_row_id" {
   routine_id      = "get_row_id"
   routine_type    = "PROCEDURE"
   language        = "SQL"
-  project                     = var.project_name
+  project                     = var.project_id
   arguments {
     name = "sequence_name"
     data_type = "{\"typeKind\" :  \"STRING\"}"
   } 
   definition_body = templatefile("${path.module}/templates/get_row_id.template", { dataset = google_bigquery_dataset.main_dataset.dataset_id })
+
+  depends_on = [ google_bigquery_dataset.main_dataset ]
 }
